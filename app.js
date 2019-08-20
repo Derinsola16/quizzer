54f3c1fddd9ba1338a766fc1913eef9fa0186472
Derin <41696799+Derinsola16@users.noreply.github.com>
2019-08-20 10:58:59 +0100

    Revert "Created the migration and model for table also created a config and s…"

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-routes(app); 
+routes(app);
 
 var server = app.listen(3000, function () {
     console.log("app running on port.", server.address().port);
--- routes/routes.js
+++ routes/routes.js
@@ -1,29 +1,7 @@
 var appRouter = function (app) {
-    // app.get("/", function (req, res) {
-    //     res.status(200).json("Welcome to our restful API");
-    // });
-
-    app.get('/users', (req, res) => {
-        return res.json('GET HTTP method on user resource');
+    app.get("/", function (req, res) {
+        res.status(200).send("Welcome to our restful API");
     });
-
-    app.post('/users', (req, res) => {
-        return res.json('POST HTTP method on user resource');
-    });
-
-    app.put('/users/:userId', (req, res) => {
-        return res.json(
-            `PUT HTTP method on user/${req.params.userId} resource`,
-        );
-    });
-
-    app.delete('/users/:userId', (req, res) => {
-        return res.json(
-            `DELETE HTTP method on user/${req.params.userId} resource`,
-        );
-    });
-
-
 }
 
 module.exports = appRouter;
