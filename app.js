65990bb736a3121df03c2e13954d3c6a371765ca
derinsola16 <aderinsolaodusnay@gmail.com>
2019-09-05 12:18:45 +0100

    add cors for browser

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var routes = require("./server/routes");
+var cors = require("cors")
 
 var app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
